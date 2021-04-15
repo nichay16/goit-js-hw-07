3ebfc91151109666423ea0c69663cd17d5dbc60b
Evgeny Nichay <nichaj16@gmail.com>
2021-04-15 09:24:19 +0300

    7-8

--- js/task-07.js
+++ js/task-07.js
@@ -1,13 +1,10 @@
 // Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
 
-const controls = document.querySelector('#font-size-control');
+const refs = {
+  inputId: document.getElementById('font-size-control'),
+  spanId: document.getElementById('text'),
+};
 
-const text = document.querySelector('#text');
-
-console.log(controls);
-console.log(text);
-
-controls.addEventListener('input', event => {
-  console.log(event.currentTarget.value);
-  text.style.fontSize = event.currentTarget.value + 'px';
+refs.inputId.addEventListener('input', size => {
+  refs.spanId.style.fontSize = `${size.currentTarget.value}px`;
 });
--- js/task-08.js
+++ js/task-08.js
@@ -1,49 +1,37 @@
-// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
+const refs = {
+  renderBtn: document.querySelector('[data-action="render"]'),
+  destroyBtn: document.querySelector('[data-action="destroy"]'),
 
-// Создай функцию createBoxes(amount), которая принимает 1 параметр amount - число. Функция создает столько div, сколько указано в amount и добавляет их в div#boxes.
+  boxesId: document.getElementById('boxes'),
+  inputEl: document.querySelector('#controls input'),
+  basicSize: 30,
+};
 
-// Каждый созданный div:
+refs.renderBtn.addEventListener('click', getAmount);
+refs.destroyBtn.addEventListener('click', removeBoxes);
 
-// Имеет случайный rgb цвет фона
-// Размеры самого первого div - 30px на 30px
-// Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
-// Создай функцию destroyBoxes(), которая очищает div#boxes.
-
-const controls = document.querySelector('#controls');
-
-const renderBtn = document.querySelector('[data-action="render"]');
-
-const destroyBtn = document.querySelector('[data-action="destroy"]');
-
-const boxesContainer = document.querySelector('#boxes');
-
-// console.log(renderBtn);
-// console.log(destroyBtn);
-// console.log(boxesContainer);
-// console.log(controls.firstElementChild.value)
-
-renderBtn.addEventListener('click', event => {
-  const amount = controls.firstElementChild.value;
+function getAmount() {
+  const amount = refs.inputEl.value;
   createBoxes(amount);
-});
+}
 
 function createBoxes(amount) {
-  let size = 30;
-  for (let i = 1; i <= amount; i += 1) {
-    const newBox = document.createElement('div');
-    boxesContainer.append(newBox);
-    newBox.style.width = `${size}px`;
-    newBox.style.height = `${size}px`;
-    newBox.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`;
-    size += 10;
-  }
+  for (let i = 0; i < amount; i++) {
+    const createDiv = document.createElement('div');
 
-  function random() {
-    return Math.floor(Math.random() * 256);
+    const randomColor = () => Math.floor(Math.random() * 256);
+
+    createDiv.style.width = `${refs.basicSize}px`;
+    createDiv.style.height = `${refs.basicSize}px`;
+    createDiv.style.backgroundColor = `rgb( ${randomColor()} , ${randomColor()} , ${randomColor()} )`;
+
+    refs.boxesId.append(createDiv);
+    refs.basicSize += 10;
   }
 }
 
-destroyBtn.addEventListener('click', event => {
-  boxesContainer.textContent = '';
-  controls.firstElementChild.value = '';
-});
+function removeBoxes() {
+  refs.boxesId.innerHTML = '';
+  refs.inputEl.value = '';
+  refs.basicSize = 30;
+}
